13b9e3cd29d3e74bb35242964eb139ad8d34cabf
Jack Gamboa <jgamboa929@gmail.com>
2020-09-15 16:38:31 -0500

    Initial commit - comlete version map with markers.

---
+++ README.md
@@ -0,0 +1,21 @@
+## Custom Google Map
+
+Simple TypeScript displaying markers on a Google map.
+
+###### Main Classes
+
+- Company
+- User
+- CustomMap
+
+###### To run locally
+
+- parcel index.html
+
+###### Language and modules
+
+:coffee: **_TypeScript_**
+
+:coffee: **_faker_**
+
+:coffee: **_Maps JavaScript API_**
---
+++ src/CustomMap.ts
@@ -0,0 +1,64 @@
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+// Instruction to every other class on how they can be an argument to addMarker() method.
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  // Note that TS behind the scenes Implicit Type Checks the objects pass as arguments to addMarker() method.
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
+
+/* // Ref only
+  // addUserMarker(user: User): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng,
+  //     },
+  //   });
+  // }
+
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng,
+  //     },
+  //   });
+  // }
+*/
